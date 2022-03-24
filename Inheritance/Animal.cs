cb6d355dd9a5ebdbcbe407f4eb5d76d11ba292a6
Stephen Cunningham <stevecunningham@outlook.com>
2022-03-23 23:36:58 -0500

    finished commit

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public string noise { get; set; }
+        public int runningSpeed { get; set; }
+        public string skinColor { get; set; }
+        public int legNum { get; set; }
+
+    }
+    public class Bird: Animal
+    {
+        public bool canFly  { get; set; }
+        public bool canSwin { get; set; }
+        public bool isNocturnal { get; set; }
+        public int wingSpan { get; set; }
+
+    }
+    public class Reptile : Animal
+    {
+        public bool coldBlood { get; set; }
+
+        public bool liveInSwamp { get; set; }
+        public int fangCount { get; set; }
+        public bool layEggs { get; set; }
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -28,12 +28,32 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird eagle = new Bird();
 
+            eagle.legNum = 2;
+            eagle.canFly = true;
+            eagle.isNocturnal = false;
+            eagle.noise = "kaww";
+            eagle.runningSpeed = 22;
+            Console.WriteLine($"The Eagle has {eagle.legNum} legs and makes the {eagle.noise} sound. Eagles rarely run, but when they do, its {eagle.runningSpeed} mph");
+            Console.WriteLine($"The fact that eagles can fly is {eagle.canFly} and nocturnal is {eagle.isNocturnal}. ");
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
              * Creatively display the class member values 
              */
-        }
+            Reptile lizard  = new Reptile();
+            lizard.noise = "hss";
+            lizard.runningSpeed = 12;
+            lizard.fangCount = 1;
+            lizard.coldBlood = true;
+            lizard.legNum = 4;
+            lizard.liveInSwamp = true;
+            Console.WriteLine($"Lizards have {lizard.legNum} legs and have a fang count of {lizard.fangCount}.");
+            Console.WriteLine($"Although quiet, lizards make the {lizard.noise} noise when provoked. Lizards run at a whopping {lizard.runningSpeed}.");
+            Console.WriteLine($"The fact that lizards live in swamps as cold-blooded monsters is { lizard.coldBlood}");
+
+            
+        }   
     }
 }
